b0bee6c58b28c4902989d00becb96f1ad7767bab
Viktor <filyk1010@gmail.com>
2023-12-16 19:27:59 +0200

    Fix ESlint cover up files of specific extensions during commit

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,5 +4,5 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
 
 module.exports = {
-  '*': ['prettier --write --list-different', buildEslintCommand],
+  '*.{js,jsx,ts,tsx}': ['prettier --write --list-different', buildEslintCommand],
 }
