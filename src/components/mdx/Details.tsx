860f80b647c63435355b1947dc7686ed8bf8ba7e
Viktor <filyk1010@gmail.com>
2023-12-16 19:25:03 +0200

    Add Details component for MDX

--- src/components/mdx/Details.tsx
+++ src/components/mdx/Details.tsx
@@ -5,7 +5,7 @@ type DetailsProps = {
 
 export default function Details({ children, title }: DetailsProps) {
   return (
-    <details className='not-prose rounded bg-gray-100 p-4 hover:cursor-pointer'>
+    <details className='not-prose rounded bg-gray-100 p-4 hover:cursor-pointer dark:bg-gray-800'>
       <summary>{title}</summary>
       {children}
     </details>
--- src/components/mdx/note.tsx
+++ src/components/mdx/note.tsx
@@ -6,10 +6,10 @@ type NoteProps = {
 
 export default function Note({ children }: NoteProps) {
   return (
-    <div className='not-prose relative my-4 border border-gray-200 bg-gray-100 px-6 py-4 text-black dark:border-gray-500 dark:bg-gray-600 dark:text-white'>
+    <div className='not-prose relative my-4 bg-gray-100 px-6 py-4 text-black dark:border-gray-500 dark:bg-gray-800 dark:text-gray-200'>
       <p className='font-bold'>Note:</p>
       <div className='break-words'>{children}</div>
-      <Info className='info-icon-custom absolute left-[calc(0%-12px)] top-[calc(0%-12px)] text-gray-400 dark:text-white' />
+      <Info className='info-icon-custom absolute left-[calc(0%-12px)] top-[calc(0%-12px)] text-gray-400 dark:text-gray-400' />
     </div>
   )
 }
