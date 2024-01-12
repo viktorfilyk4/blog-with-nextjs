07d926762818b18f258b2410b9bca25c9b01a13c
Viktor <filyk1010@gmail.com>
2024-01-12 15:05:43 +0200

    Modifying 'a' component for MDX

--- src/components/mdx/markdown-components.tsx
+++ src/components/mdx/markdown-components.tsx
@@ -2,15 +2,21 @@ import { MDXComponents } from 'mdx/types'
 import { Code } from 'bright'
 import Note from './Note'
 import Details from './Details'
+import Link from 'next/link'
 
 export const mdxComponents: MDXComponents = {
-  // a: ({ children, ...props }) => {
-  //   return (
-  //     <Link {...props} href={props.href || ""}>
-  //       {children}
-  //     </Link>
-  //   )
-  // },
+  a: ({ children, ...props }) => {
+    return (
+      <Link
+        {...props}
+        href={props.href || ''}
+        ref={undefined}
+        className='text-blue-500 hover:underline'
+      >
+        {children}
+      </Link>
+    )
+  },
   // img: ({ children, props }) => {
   //   // You need to do some work here to get the width and height of the image.
   //   // See the details below for my solution.
